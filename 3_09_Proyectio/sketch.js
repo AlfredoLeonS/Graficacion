0daea9a5f7715b5f73b03888734a32b4c0fd35ff
AlfredoLeonS <L24170596@CULIACAN.TECNM.MX>
2026-04-29 03:34:04 +0000

    proyecto_final_3

--- 3_09_Proyectio/sketch.js
+++ 3_09_Proyectio/sketch.js
@@ -1,18 +1,39 @@
+let modo = true;
+let fov = 3.1416/3;
+
 function setup() {
- createCanvas(600, 400);
+ createCanvas(700, 500, WEBGL);
 }
 function draw(){
  background(240); 
- let O = map(mouseX,0,width,0,TWO_PI)
- s = (distancia)
- push()
- translate(width/2, height/2);
- scale(s);
- rotate(O);
- textAlign(CENTER,CENTER);
- textSize(40);
- text("Rotado", 0, 0);
-  pop()
+ orbitControl();
+ if (modo) {
+ perspective(fov, width/height, 0.1, 1000);
+ } else {
+ ortho();
+ }
+ // Cubo 
+ push();
+ translate(-100, 0, 0);
+ fill(0,200,0)
+ box(80);
+ pop();
+ 
+ push();
+ translate(150, 0, -100);
+ rotateZ(frameCount * 0.02);
+ sphere(80);
+ pop();
+
+ push();
+ translate(-300, 0, 100);
+ rotateY(frameCount * 0.02);
+ cone(50, 120);
+ pop();
  
 }
-
+function keyPressed() {
+ if (key === 's' || key === 'S') modo = !modo;
+ if (key === 'a' || key === 'A') fov -= 0.1;
+ if (key === 'd' || key === 'D') fov += 0.1
+}
